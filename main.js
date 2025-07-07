d7f444384946590edf503cb5a1e8565b355b1076
purvapangarkar <purvap46@gmail.com>
2025-07-07 15:26:05 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('form-message').textContent = "Thank you! Your message has been received.";
-  this.reset();
-});
-
