e05534121163594683b1746a1a763dfc3b97d422
purvapangarkar <purvap46@gmail.com>
2025-07-07 15:27:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+  .then(() => messaging.getToken())
+  .then(token => {
+    console.log("Push token:", token);
+  })
+  .catch(err => console.error("Push permission denied", err));
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(() => console.log("Service Worker Registered"))
+    .catch(err => console.error("SW Registration Failed:", err));
+}
